07e484a2a86888b608c476f900d4e761dbff5cc4
cyberDay777 <asacademybhind@gmail.com>
2025-05-06 18:04:31 +0530

    Update twrp_chenfeng.mk

--- twrp_chenfeng.mk
+++ twrp_chenfeng.mk
@@ -13,14 +13,14 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 PRODUCT_RELEASE_NAME := chenfing
 
 ## Device identifier
-PRODUCT_DEVICE := chenfing
-PRODUCT_NAME := twrp_chenfing
+PRODUCT_DEVICE := chenfeng
+PRODUCT_NAME := twrp_chenfeng
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := civi 14 pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := chenfing
+TARGET_OTA_ASSERT_DEVICE := chenfeng
 
 # Theme
 TW_STATUS_ICONS_ALIGN := center
