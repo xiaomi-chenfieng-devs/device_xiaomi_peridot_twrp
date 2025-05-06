72381b0fd112967330b52fd8122793d8b444a8c2
cyberDay777 <asacademybhind@gmail.com>
2025-05-06 18:04:44 +0530

    Update twrp_chenfeng.mk

--- twrp_chenfeng.mk
+++ twrp_chenfeng.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/chenfing
+DEVICE_PATH := device/xiaomi/chenfeng
 
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
